49b5c802ef8a456a3e122e47fe8c4d6cd4acec64
catalinbroinas <catalinbroinas@yahoo.com>
2026-06-25 18:48:00 +0300

    fix(navbar): fix Font Awesome usage

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@ import '../scss/main.scss';
 
 import { Ripple, Collapse, initMDB } from 'mdb-ui-kit/js/mdb.es.min.js';
 
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
 function MainDomManager() {
     const initApp = () => {
         initMDB({ Ripple, Collapse });
