403ce5d6446f39a87a48b8d7654f26f4760b3f7e
catalinbroinas <catalinbroinas@yahoo.com>
2026-06-30 18:41:20 +0300

    refactor(main.js): improve readability

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,32 @@
+// SCSS
 import '../scss/main.scss';
 
+// MDB
 import { Ripple, Collapse, initMDB } from 'mdb-ui-kit/js/mdb.es.min.js';
 
+// Font Awesome
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
+// Modules
 import { NavbarManager } from './modules/dom/navbar';
 
 function MainDomManager() {
-    const addEvents = () => {
-        const navbar = document.querySelector('#navbar');
-        const navbarManager = NavbarManager(navbar);
+  const addEvents = () => {
+    const navbar = document.querySelector('#navbar');
+    const navbarManager = NavbarManager(navbar);
 
-        navbarManager.scrollNavbar();
-    };
+    navbarManager.scrollNavbar();
+  };
 
-    const initApp = () => {
-        initMDB({ Ripple, Collapse });
-        addEvents();
-    };
+  const initApp = () => {
+    initMDB({ Ripple, Collapse });
+    addEvents();
+  };
 
-    return { initApp };
+  return { initApp };
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const domManager = MainDomManager();
-    domManager.initApp();
+  const domManager = MainDomManager();
+  domManager.initApp();
 });
