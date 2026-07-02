8f3ee0c8348d749a807b033ba73b842bbb23a680
catalinbroinas <catalinbroinas@yahoo.com>
2026-07-02 20:03:55 +0300

    feat(data): add initial teams data

---
+++ src/js/react/data/teamImages.js
@@ -0,0 +1,9 @@
+import algeriaImg from "../../../assets/images/countries/algeria.webp";
+import belgiumImg from "../../../assets/images/countries/belgium.webp";
+import panamaImg from "../../../assets/images/countries/panama.webp";
+
+export const teamImages = {
+  algeria: algeriaImg,
+  belgium: belgiumImg,
+  panama: panamaImg
+};
---
+++ src/js/react/data/teams.js
@@ -0,0 +1,25 @@
+import { teamImages } from "./teamImages";
+
+export const teams = [
+  {
+    id: "belgium",
+    imageUrl: teamImages.belgium,
+    imageAlt: "Mons, Région Wallonne, Belgique",
+    name: "Belgium",
+    flag: "belgium"
+  },
+  {
+    id: "panama",
+    imageUrl: teamImages.panama,
+    imageAlt: "Panama City, Panama",
+    name: "Panama",
+    flag: "panama"
+  },
+  {
+    id: "algeria",
+    imageUrl: teamImages.algeria,
+    imageAlt: "Alger Centre, Wilaya d'Alger, Algérie,",
+    name: "Algeria",
+    flag: "algeria"
+  },
+];
