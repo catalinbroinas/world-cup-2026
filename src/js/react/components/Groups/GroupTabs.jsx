3d1a39c8b419ace971d9864bfe44dc4f30c845b0
catalinbroinas <catalinbroinas@yahoo.com>
2026-07-03 19:40:26 +0300

    feat(GroupTabs): add group navigation component

---
+++ src/js/react/components/Groups/GroupTabs.jsx
@@ -0,0 +1,28 @@
+
+function GroupTabs({ groups, activeTab, onTabChange }) {
+  return (
+    <ul className="nav nav-tabs nav-fill group-tabs" role="tablist">
+      {groups.map((group) => (
+        <li
+          key={group.id}
+          className="nav-item group-tabs__item"
+          role="presentation"
+        >
+          <button
+            type="button"
+            role="tab"
+            aria-selected={group.id === activeTab}
+            className={`nav-link group-tabs__link${
+              group.id === activeTab ? " active" : ""
+            }`}
+            onClick={() => onTabChange(group.id)}
+          >
+            {group.id}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default GroupTabs;
