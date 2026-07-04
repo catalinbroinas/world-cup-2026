44b03c8f2d1a5f6c1f69841af92867ff7bfc9023
catalinbroinas <catalinbroinas@yahoo.com>
2026-07-04 18:31:31 +0300

    feat(data): add full group table data

---
+++ src/js/react/data/groups.js
@@ -0,0 +1,555 @@
+
+export const groups = [
+  {
+    id: "A",
+    name: "Group A",
+    teams: [
+      {
+        id: "mexico",
+        country: "Mexico",
+        flag: "mexico",
+        W: 3,
+        D: 0,
+        L: 0,
+        GF: 6,
+        GA: 0
+      },
+      {
+        id: "south-africa",
+        country: "South Africa",
+        flag: "south-africa",
+        W: 1,
+        D: 1,
+        L: 1,
+        GF: 2,
+        GA: 3
+      },
+      {
+        id: "south-korea",
+        country: "South Korea",
+        flag: "south-korea",
+        W: 1,
+        D: 0,
+        L: 2,
+        GF: 2,
+        GA: 3
+      },
+      {
+        id: "czech-republic",
+        country: "Czech Republic",
+        flag: "czech-republic",
+        W: 0,
+        D: 1,
+        L: 2,
+        GF: 2,
+        GA: 6
+      }
+    ]
+  },
+  {
+    id: "B",
+    name: "Group B",
+    teams: [
+      {
+        id: "switzerland",
+        country: "Switzerland",
+        flag: "switzerland",
+        W: 2,
+        D: 1,
+        L: 0,
+        GF: 7,
+        GA: 3
+      },
+      {
+        id: "qatar",
+        country: "Qatar",
+        flag: "qatar",
+        W: 0,
+        D: 1,
+        L: 2,
+        GF: 2,
+        GA: 10
+      },
+      {
+        id: "canada",
+        country: "Canada",
+        flag: "canada",
+        W: 1,
+        D: 1,
+        L: 1,
+        GF: 8,
+        GA: 3
+      },
+      {
+        id: "bosnia",
+        country: "Bosnia",
+        flag: "bosnia",
+        W: 1,
+        D: 1,
+        L: 1,
+        GF: 5,
+        GA: 6
+      }
+    ]
+  },
+  {
+    id: "C",
+    name: "Group C",
+    teams: [
+      {
+        id: "scotland",
+        country: "Scotland",
+        flag: "scotland",
+        W: 1,
+        D: 0,
+        L: 2,
+        GF: 1,
+        GA: 4
+      },
+      {
+        id: "brazil",
+        country: "Brazil",
+        flag: "brazil",
+        W: 2,
+        D: 1,
+        L: 0,
+        GF: 7,
+        GA: 1
+      },
+      {
+        id: "haiti",
+        country: "Haiti",
+        flag: "haiti",
+        W: 0,
+        D: 0,
+        L: 3,
+        GF: 2,
+        GA: 8
+      },
+      {
+        id: "morocco",
+        country: "Morocco",
+        flag: "morocco",
+        W: 2,
+        D: 1,
+        L: 0,
+        GF: 6,
+        GA: 3
+      }
+    ]
+  },
+  {
+    id: "D",
+    name: "Group D",
+    teams: [
+      {
+        id: "united-states",
+        country: "SUA",
+        flag: "united-states",
+        W: 2,
+        D: 0,
+        L: 1,
+        GF: 8,
+        GA: 4
+      },
+      {
+        id: "turkey",
+        country: "Turkey",
+        flag: "turkey",
+        W: 1,
+        D: 0,
+        L: 2,
+        GF: 3,
+        GA: 5
+      },
+      {
+        id: "australia",
+        country: "Australia",
+        flag: "australia",
+        W: 1,
+        D: 1,
+        L: 1,
+        GF: 2,
+        GA: 2
+      },
+      {
+        id: "paraguay",
+        country: "Paraguay",
+        flag: "paraguay",
+        W: 1,
+        D: 1,
+        L: 1,
+        GF: 2,
+        GA: 4
+      }
+    ]
+  },
+  {
+    id: "E",
+    name: "Group E",
+    teams: [
+      {
+        id: "germany",
+        country: "Germany",
+        flag: "germany",
+        W: 2,
+        D: 0,
+        L: 1,
+        GF: 10,
+        GA: 4
+      },
+      {
+        id: "ecuador",
+        country: "Ecuador",
+        flag: "ecuador",
+        W: 1,
+        D: 1,
+        L: 1,
+        GF: 2,
+        GA: 2
+      },
+      {
+        id: "cote-divoire",
+        country: "Cote Divoire",
+        flag: "cote-divoire",
+        W: 2,
+        D: 0,
+        L: 1,
+        GF: 4,
+        GA: 2
+      },
+      {
+        id: "cook-islands",
+        country: "Coracao",
+        flag: "cook-islands",
+        W: 0,
+        D: 1,
+        L: 2,
+        GF: 1,
+        GA: 9
+      }
+    ]
+  },
+  {
+    id: "F",
+    name: "Group F",
+    teams: [
+      {
+        id: "sweden",
+        country: "Sweden",
+        flag: "sweden",
+        W: 1,
+        D: 1,
+        L: 1,
+        GF: 7,
+        GA: 7
+      },
+      {
+        id: "japan",
+        country: "Japan",
+        flag: "japan",
+        W: 1,
+        D: 2,
+        L: 0,
+        GF: 7,
+        GA: 3
+      },
+      {
+        id: "netherlands",
+        country: "Netherlands",
+        flag: "netherlands",
+        W: 2,
+        D: 1,
+        L: 0,
+        GF: 10,
+        GA: 4
+      },
+      {
+        id: "tunisia",
+        country: "Tunisia",
+        flag: "tunisia",
+        W: 0,
+        D: 0,
+        L: 3,
+        GF: 2,
+        GA: 12
+      }
+    ]
+  },
+  {
+    id: "G",
+    name: "Group G",
+    teams: [
+      {
+        id: "new-zealand",
+        country: "New Zealand",
+        flag: "new-zealand",
+        W: 0,
+        D: 1,
+        L: 2,
+        GF: 4,
+        GA: 10
+      },
+      {
+        id: "iran",
+        country: "Iran",
+        flag: "iran",
+        W: 0,
+        D: 3,
+        L: 0,
+        GF: 3,
+        GA: 3
+      },
+      {
+        id: "belgium",
+        country: "Belgium",
+        flag: "belgium",
+        W: 1,
+        D: 2,
+        L: 0,
+        GF: 6,
+        GA: 2
+      },
+      {
+        id: "egypt",
+        country: "Egypt",
+        flag: "egypt",
+        W: 1,
+        D: 2,
+        L: 0,
+        GF: 5,
+        GA: 3
+      }
+    ]
+  },
+  {
+    id: "H",
+    name: "Group H",
+    teams: [
+      {
+        id: "uruguay",
+        country: "Uruguay",
+        flag: "uruguay",
+        W: 0,
+        D: 2,
+        L: 1,
+        GF: 3,
+        GA: 4
+      },
+      {
+        id: "saudi-arabia",
+        country: "Arabia Saudi",
+        flag: "saudi-arabia",
+        W: 0,
+        D: 2,
+        L: 1,
+        GF: 1,
+        GA: 5
+      },
+      {
+        id: "spain",
+        country: "Spain",
+        flag: "spain",
+        W: 2,
+        D: 1,
+        L: 0,
+        GF: 5,
+        GA: 0
+      },
+      {
+        id: "cape-verde",
+        country: "Cape Verde",
+        flag: "cape-verde",
+        W: 0,
+        D: 3,
+        L: 0,
+        GF: 2,
+        GA: 2
+      }
+    ]
+  },
+  {
+    id: "I",
+    name: "Group I",
+    teams: [
+      {
+        id: "norway",
+        country: "Norway",
+        flag: "norway",
+        W: 2,
+        D: 0,
+        L: 1,
+        GF: 8,
+        GA: 7
+      },
+      {
+        id: "france",
+        country: "France",
+        flag: "france",
+        W: 3,
+        D: 0,
+        L: 0,
+        GF: 10,
+        GA: 2
+      },
+      {
+        id: "senegal",
+        country: "Senegal",
+        flag: "senegal",
+        W: 1,
+        D: 0,
+        L: 2,
+        GF: 8,
+        GA: 6
+      },
+      {
+        id: "iraq",
+        country: "Iraq",
+        flag: "iraq",
+        W: 0,
+        D: 0,
+        L: 3,
+        GF: 1,
+        GA: 12
+      }
+    ]
+  },
+  {
+    id: "J",
+    name: "Group J",
+    teams: [
+      {
+        id: "argentina",
+        country: "Argentina",
+        flag: "argentina",
+        W: 3,
+        D: 0,
+        L: 0,
+        GF: 8,
+        GA: 1
+      },
+      {
+        id: "austria",
+        country: "Austria",
+        flag: "austria",
+        W: 1,
+        D: 1,
+        L: 1,
+        GF: 6,
+        GA: 6
+      },
+      {
+        id: "jordan",
+        country: "Jordan",
+        flag: "jordan",
+        W: 0,
+        D: 0,
+        L: 3,
+        GF: 3,
+        GA: 8
+      },
+      {
+        id: "algeria",
+        country: "Algeria",
+        flag: "algeria",
+        W: 1,
+        D: 1,
+        L: 1,
+        GF: 5,
+        GA: 7
+      }
+    ]
+  },
+  {
+    id: "K",
+    name: "Group K",
+    teams: [
+      {
+        id: "portugal",
+        country: "Portugal",
+        flag: "portugal",
+        W: 1,
+        D: 2,
+        L: 0,
+        GF: 6,
+        GA: 1
+      },
+      {
+        id: "colombia",
+        country: "Colombia",
+        flag: "colombia",
+        W: 2,
+        D: 1,
+        L: 0,
+        GF: 4,
+        GA: 1
+      },
+      {
+        id: "congo",
+        country: "Congo DR",
+        flag: "congo",
+        W: 1,
+        D: 1,
+        L: 1,
+        GF: 4,
+        GA: 3
+      },
+      {
+        id: "uzbekistan",
+        country: "Uzbekistan",
+        flag: "uzbekistan",
+        W: 0,
+        D: 0,
+        L: 3,
+        GF: 2,
+        GA: 11
+      }
+    ]
+  },
+  {
+    id: "L",
+    name: "Group L",
+    teams: [
+      {
+        id: "croatia",
+        country: "Croatia",
+        flag: "croatia",
+        W: 2,
+        D: 0,
+        L: 1,
+        GF: 5,
+        GA: 5
+      },
+      {
+        id: "england",
+        country: "England",
+        flag: "england",
+        W: 2,
+        D: 1,
+        L: 0,
+        GF: 6,
+        GA: 2
+      },
+      {
+        id: "ghana",
+        country: "Ghana",
+        flag: "ghana",
+        W: 1,
+        D: 1,
+        L: 1,
+        GF: 2,
+        GA: 2
+      },
+      {
+        id: "panama",
+        country: "Panama",
+        flag: "panama",
+        W: 0,
+        D: 0,
+        L: 3,
+        GF: 0,
+        GA: 4
+      }
+    ]
+  }
+];
