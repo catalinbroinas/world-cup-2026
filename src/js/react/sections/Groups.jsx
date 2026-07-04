97b3a9dfe92bc079340f143390cec5b2b5bace40
catalinbroinas <catalinbroinas@yahoo.com>
2026-07-04 18:50:20 +0300

    feat(Groups): render group navigation and table

--- src/js/react/sections/Groups.jsx
+++ src/js/react/sections/Groups.jsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
+
+// Data
+import { groups } from "../data/groups";
+
+// Components
+import GroupTabs from "../components/Groups/GroupTabs";
+import GroupTable from "../components/Groups/GroupTable";
 
 function Groups() {
+  const [activeGroupId, setActiveGroupId] = useState(groups[0].id);
+
+  const selectedGroup = groups.find(
+    (group) => group.id === activeGroupId
+  );
+
   return (
-    <></>
+    <>
+      <section className="group-navigation">
+        <GroupTabs
+          groups={groups}
+          activeTab={activeGroupId}
+          onTabChange={setActiveGroupId}
+        />
+      </section>
+
+      <section className="group-stats">
+        <GroupTable group={selectedGroup} />
+      </section>
+    </>
   );
 }
 
