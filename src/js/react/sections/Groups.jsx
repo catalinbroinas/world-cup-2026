7b545d590ecc16d1f29cc04a6f7118b831e5fa3f
catalinbroinas <catalinbroinas@yahoo.com>
2026-07-07 19:21:11 +0300

    feat(Groups): render matches from data

--- src/js/react/sections/Groups.jsx
+++ src/js/react/sections/Groups.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 
 // Data
 import { groups } from "../data/groups";
+import { matches } from "../data/matches";
 
 // Components
 import GroupTabs from "../components/Groups/GroupTabs";
 import GroupTable from "../components/Groups/GroupTable";
+import MatchGrid from "../components/Matchs/MatchGrid";
 
 function Groups() {
   const [activeGroupId, setActiveGroupId] = useState(groups[0].id);
@@ -13,6 +15,10 @@ function Groups() {
   const selectedGroup = groups.find(
     (group) => group.id === activeGroupId
   );
+
+  const groupMatches = matches.filter((match) => match.groupId === activeGroupId);
+
+  const matchDay1 = groupMatches.filter((match) => match.round === 1);
 
   return (
     <>
@@ -26,6 +32,12 @@ function Groups() {
 
       <div className="group-stats">
         <GroupTable group={selectedGroup} />
+      </div>
+
+      <div className="group-matches">
+        <h3 className="subtitle mb-0">Match day 1</h3>
+        <hr className="hr mt-2 mb-4" />
+        <MatchGrid matches={matchDay1} />
       </div>
     </>
   );
