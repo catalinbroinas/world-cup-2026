ead11e232d62d43adac50f26a380c63fb77d944a
catalinbroinas <catalinbroinas@yahoo.com>
2026-07-07 19:00:14 +0300

    feat(data): add initial match data

---
+++ src/js/react/data/matches.js
@@ -0,0 +1,86 @@
+
+export const matches = [
+  // A
+  {
+    id: crypto.randomUUID(),
+    groupId: "A",
+    round: 1,
+
+    homeTeam: {
+      name: "Mexico",
+      flag: "mexico",
+      result: 2
+    },
+
+    awayTeam: {
+      name: "South Africa",
+      flag: "south-africa",
+      result: 0
+    },
+
+    stadium: "Azteca",
+    referee: "Sampaio W."
+  },
+  {
+    id: crypto.randomUUID(),
+    groupId: "A",
+    round: 1,
+
+    homeTeam: {
+      name: "South Korea",
+      flag: "south-korea",
+      result: 2
+    },
+
+    awayTeam: {
+      name: "Czech Republic",
+      flag: "czech-republic",
+      result: 1
+    },
+
+    stadium: "Estadio Akron",
+    referee: "Omar A."
+  },
+
+  // B
+  {
+    id: crypto.randomUUID(),
+    groupId: "B",
+    round: 1,
+
+    homeTeam: {
+      name: "Canada",
+      flag: "canada",
+      result: 1
+    },
+
+    awayTeam: {
+      name: "Bosnia",
+      flag: "bosnia",
+      result: 1
+    },
+
+    stadium: "BMO Field",
+    referee: "Tello F"
+  },
+  {
+    id: crypto.randomUUID(),
+    groupId: "B",
+    round: 1,
+
+    homeTeam: {
+      name: "Switzerland",
+      flag: "switzerland",
+      result: 1
+    },
+
+    awayTeam: {
+      name: "Qatar",
+      flag: "qatar",
+      result: 1
+    },
+
+    stadium: "Levi's Stadium",
+    referee: "Martinez S"
+  },
+];
