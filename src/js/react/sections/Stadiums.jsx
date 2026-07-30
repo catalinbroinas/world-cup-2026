6c8048d1b574f923805c5e999dc4129ea0d7065f
catalinbroinas <catalinbroinas@yahoo.com>
2026-07-30 19:15:42 +0300

    refactor(Stadiums): add semantic section heading

--- src/js/react/sections/Stadiums.jsx
+++ src/js/react/sections/Stadiums.jsx
@@ -5,6 +5,10 @@ import StadiumGrid from "../components/Stadiums/StadiumGrid";
 function Stadiums() {
   return (
     <section className="stadiums-content">
+      <h2 className="visually-hidden">
+        Tournament stadiums
+      </h2>
+
       <StadiumGrid stadiums={stadiums} />
     </section>
   );
