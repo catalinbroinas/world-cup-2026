4ff0ac202aeb9fa2271b644408ba05c0962ff4ef
catalinbroinas <catalinbroinas@yahoo.com>
2026-08-04 20:05:09 +0300

    feat(data): add 6 new teams

--- src/js/data/teams/teamImages.js
+++ src/js/data/teams/teamImages.js
@@ -1,9 +1,21 @@
 import wembleyImg from "../../../assets/images/stadiums/wembley.webp";
 import olympiastadionImg from "../../../assets/images/stadiums/olympiastadion.webp";
 import bernabeuImg from "../../../assets/images/stadiums/bernabeu.webp";
+import kingBImg from "../../../assets/images/stadiums/kingB.webp";
+import benficaImg from "../../../assets/images/stadiums/benfica.webp";
+import parcDesPrincesImg from "../../../assets/images/stadiums/parcDesPrinces.webp";
+import deKuipImg from "../../../assets/images/stadiums/deKuip.webp";
+import celticParkImg from "../../../assets/images/stadiums/celticPark.webp";
+import bernImg from "../../../assets/images/stadiums/bern.webp";
 
 export const teamImages = {
   wembley: wembleyImg,
   olympiastadion: olympiastadionImg,
-  bernabeu: bernabeuImg
+  bernabeu: bernabeuImg,
+  kingB: kingBImg,
+  benfica: benficaImg,
+  parcDesPrinces: parcDesPrincesImg,
+  deKuip: deKuipImg,
+  celticPark: celticParkImg,
+  bern: bernImg,
 };
--- src/js/data/teams/teams.js
+++ src/js/data/teams/teams.js
@@ -22,4 +22,46 @@ export const teams = [
     name: "Germany",
     flag: "germany"
   },
+  {
+    id: "belgium",
+    imageUrl: teamImages.kingB,
+    imageAlt: "King Baudouin",
+    name: "Belgium",
+    flag: "belgium"
+  },
+  {
+    id: "france",
+    imageUrl: teamImages.parcDesPrinces,
+    imageAlt: "Parc Des Princes",
+    name: "France",
+    flag: "france"
+  },
+  {
+    id: "portugal",
+    imageUrl: teamImages.benfica,
+    imageAlt: "Estádio da Luz",
+    name: "Portugal",
+    flag: "portugal"
+  },
+  {
+    id: "switzerland",
+    imageUrl: teamImages.bern,
+    imageAlt: "Stadion Wankdorf",
+    name: "Switzerland",
+    flag: "switzerland"
+  },
+  {
+    id: "netherlands",
+    imageUrl: teamImages.deKuip,
+    imageAlt: "De Kuip",
+    name: "Netherlands",
+    flag: "netherlands"
+  },
+  {
+    id: "scotland",
+    imageUrl: teamImages.celticPark,
+    imageAlt: "Celtic Park",
+    name: "Scotland",
+    flag: "scotland"
+  },
 ];
