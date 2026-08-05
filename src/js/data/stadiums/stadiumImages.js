4b8585aa56081fa3d94a4649197624aff7c150c1
catalinbroinas <catalinbroinas@yahoo.com>
2026-08-05 18:54:49 +0300

    feat(data): add 4 new stadiums

--- src/js/data/stadiums/stadiumImages.js
+++ src/js/data/stadiums/stadiumImages.js
@@ -1,5 +1,13 @@
 import akronImg from "../../../assets/images/stadiums/akron.webp";
+import aztecaImg from "../../../assets/images/stadiums/azteca.webp";
+import bbvaImg from "../../../assets/images/stadiums/bbva.webp";
+import bcPlaceImg from "../../../assets/images/stadiums/bcPlace.webp";
+import bmoFieldImg from "../../../assets/images/stadiums/bmoField.webp";
 
 export const stadiumImages = {
-  akron: akronImg
+  akron: akronImg,
+  azteca: aztecaImg,
+  bbva: bbvaImg,
+  bcPlace: bcPlaceImg,
+  bmoField: bmoFieldImg
 };
--- src/js/data/stadiums/stadiums.js
+++ src/js/data/stadiums/stadiums.js
@@ -8,5 +8,37 @@ export const stadiums = [
     name: "Estadio Akron",
     capacity: 45.664,
     matches: 4
+  },
+  {
+    id: crypto.randomUUID(),
+    imageUrl: stadiumImages.bbva,
+    location: "Monterey, Mexico",
+    name: "Estadio BBVA",
+    capacity: 51.243,
+    matches: 4
+  },
+  {
+    id: crypto.randomUUID(),
+    imageUrl: stadiumImages.azteca,
+    location: "Mexico City, Mexico",
+    name: "Estadio Azteca",
+    capacity: 80.824,
+    matches: 5
+  },
+  {
+    id: crypto.randomUUID(),
+    imageUrl: stadiumImages.bcPlace,
+    location: "Vancouver, Canada",
+    name: "BC Place",
+    capacity: 52.497,
+    matches: 7
+  },
+  {
+    id: crypto.randomUUID(),
+    imageUrl: stadiumImages.bmoField,
+    location: "Toronto, Canada",
+    name: "BMO Field",
+    capacity: 43.036,
+    matches: 6
   }
 ];
