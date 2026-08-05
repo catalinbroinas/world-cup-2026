d246e53228692e8b512300a00b87f911d98b9171
catalinbroinas <catalinbroinas@yahoo.com>
2026-08-05 19:22:54 +0300

    feat(data): add knockout stage matches

--- src/js/data/matches/knockoutMatches.js
+++ src/js/data/matches/knockoutMatches.js
@@ -77,7 +77,6 @@ export const knockoutMatches = [
     referee: "Jayed J.",
     date: "2026-06-29"
   },
-
   {
     id: crypto.randomUUID(),
     stage: KNOCKOUT_STAGE.ROUND_OF_32.id,
@@ -103,5 +102,225 @@ export const knockoutMatches = [
     stadium: "Lumen Field",
     referee: "Martinez S.",
     date: "2026-07-01"
+  },
+
+  // Round of 16
+  {
+    id: crypto.randomUUID(),
+    stage: KNOCKOUT_STAGE.ROUND_OF_16.id,
+
+    homeTeam: {
+      name: "Canada",
+      flag: "canada",
+      result: 0
+    },
+
+    awayTeam: {
+      name: "Morocco",
+      flag: "morocco",
+      result: 3
+    },
+
+    extraTime: null,
+    penalties: null,
+
+    stadium: "NRG Stadium",
+    referee: "Oliver M..",
+    date: "2026-07-04"
+  },
+  {
+    id: crypto.randomUUID(),
+    stage: KNOCKOUT_STAGE.ROUND_OF_16.id,
+
+    homeTeam: {
+      name: "Paraguay",
+      flag: "paraguay",
+      result: 0
+    },
+
+    awayTeam: {
+      name: "France",
+      flag: "france",
+      result: 1
+    },
+
+    extraTime: null,
+    penalties: null,
+
+    stadium: "Lincoln Financial Field",
+    referee: "Tantashev I.",
+    date: "2026-07-05"
+  },
+  {
+    id: crypto.randomUUID(),
+    stage: KNOCKOUT_STAGE.ROUND_OF_16.id,
+
+    homeTeam: {
+      name: "Brazil",
+      flag: "brazil",
+      result: 1
+    },
+
+    awayTeam: {
+      name: "Norway",
+      flag: "norway",
+      result: 2
+    },
+
+    extraTime: null,
+    penalties: null,
+
+    stadium: "MetLife Stadium",
+    referee: "Elfath I.",
+    date: "2026-07-05"
+  },
+
+  // Quarter-finals
+  {
+    id: crypto.randomUUID(),
+    stage: KNOCKOUT_STAGE.QUARTER_FINAL.id,
+
+    homeTeam: {
+      name: "France",
+      flag: "france",
+      result: 2
+    },
+
+    awayTeam: {
+      name: "Morocco",
+      flag: "morocco",
+      result: 0
+    },
+
+    extraTime: null,
+    penalties: null,
+
+    stadium: "Gillette Stadium",
+    referee: "Tello F.",
+    date: "2026-07-09"
+  },
+  {
+    id: crypto.randomUUID(),
+    stage: KNOCKOUT_STAGE.QUARTER_FINAL.id,
+
+    homeTeam: {
+      name: "Spain",
+      flag: "spain",
+      result: 2
+    },
+
+    awayTeam: {
+      name: "Belgium",
+      flag: "belgium",
+      result: 1
+    },
+
+    extraTime: null,
+    penalties: null,
+
+    stadium: "SoFi Stadium",
+    referee: "Oliver M.",
+    date: "2026-07-10"
+  },
+
+  // Semi-finals
+  {
+    id: crypto.randomUUID(),
+    stage: KNOCKOUT_STAGE.SEMI_FINAL.id,
+
+    homeTeam: {
+      name: "France",
+      flag: "france",
+      result: 0
+    },
+
+    awayTeam: {
+      name: "Spain",
+      flag: "spain",
+      result: 2
+    },
+
+    extraTime: null,
+    penalties: null,
+
+    stadium: "AT&T Stadium",
+    referee: "Barton I.",
+    date: "2026-07-14"
+  },
+  {
+    id: crypto.randomUUID(),
+    stage: KNOCKOUT_STAGE.SEMI_FINAL.id,
+
+    homeTeam: {
+      name: "England",
+      flag: "england",
+      result: 1
+    },
+
+    awayTeam: {
+      name: "Argentina",
+      flag: "argentina",
+      result: 2
+    },
+
+    extraTime: null,
+    penalties: null,
+
+    stadium: "Mercedes-Benz",
+    referee: "Elfath I.",
+    date: "2026-07-15"
+  },
+
+  // Third Place
+  {
+    id: crypto.randomUUID(),
+    stage: KNOCKOUT_STAGE.THIRD_PLACE.id,
+
+    homeTeam: {
+      name: "France",
+      flag: "france",
+      result: 4
+    },
+
+    awayTeam: {
+      name: "England",
+      flag: "england",
+      result: 6
+    },
+
+    extraTime: null,
+    penalties: null,
+
+    stadium: "Hard Rock Stadium",
+    referee: "Valenzuela J.",
+    date: "2026-07-19"
+  },
+
+  // Final
+  {
+    id: crypto.randomUUID(),
+    stage: KNOCKOUT_STAGE.FINAL.id,
+
+    homeTeam: {
+      name: "Spain",
+      flag: "spain",
+      result: 0
+    },
+
+    awayTeam: {
+      name: "Argentina",
+      flag: "argentina",
+      result: 0
+    },
+
+    extraTime: {
+      homeResult: 1,
+      awayResult: 0
+    },
+    penalties: null,
+
+    stadium: "MetLife Stadium",
+    referee: "Vincic S.",
+    date: "2026-07-19"
   }
 ];
