a941d66860fba707d60614383bf79522916ae701
catalinbroinas <catalinbroinas@yahoo.com>
2026-08-14 16:42:51 +0300

    style(Stadiums): improve layout on mobile

--- src/js/react/components/Stadiums/StadiumGrid.jsx
+++ src/js/react/components/Stadiums/StadiumGrid.jsx
@@ -2,7 +2,7 @@ import StadiumCard from "./StadiumCard";
 
 function StadiumGrid({ stadiums }) {
   return (
-    <div className="row row-cols-xl-2 row-cols-1 gy-5">
+    <div className="row row-cols-xl-2 row-cols-1 gy-sm-5">
       {stadiums.map((stadium) => (
         <div key={stadium.id} className="col">
           <StadiumCard stadium={stadium} />
