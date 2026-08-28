71982f3c949bf6a2e8cb4d76132c4cd1f60232b3
catalinbroinas <catalinbroinas@yahoo.com>
2026-08-28 20:14:42 +0300

    feat(Stadiums): add search bar

--- src/js/react/sections/Stadiums.jsx
+++ src/js/react/sections/Stadiums.jsx
@@ -1,15 +1,38 @@
+import { useState } from "react";
+
 import { stadiums } from "../../data/stadiums/stadiums";
 
 import StadiumGrid from "../components/Stadiums/StadiumGrid";
+import SearchBar from "../components/Toolbar/SearchBar";
 
 function Stadiums() {
+  const [query, setQuery] = useState("");
+
+  const filteredStadiums = stadiums.filter((stadium =>
+    stadium.name.toLowerCase().includes(query.toLowerCase())
+  ));
+
   return (
     <section className="stadiums-content">
       <h2 className="visually-hidden">
         Tournament stadiums
       </h2>
 
-      <StadiumGrid stadiums={stadiums} />
+      <div className="toolbar">
+        <SearchBar
+          name="stadiums"
+          query={query}
+          onQueryChange={setQuery}
+        />
+      </div>
+
+      {filteredStadiums.length > 0 ? (
+        <StadiumGrid stadiums={filteredStadiums} />
+      ) : (
+        <div className="alert alert-info" role="status">
+          No stadiums found.
+        </div>
+      )}
     </section>
   );
 }
