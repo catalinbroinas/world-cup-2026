e9b870f8138b58b20512f33b4c2668c2ed684ec6
catalinbroinas <catalinbroinas@yahoo.com>
2026-09-22 14:25:51 +0300

    content(data): reorder teams by confederation

--- src/js/data/teams/teams.js
+++ src/js/data/teams/teams.js
@@ -64,50 +64,6 @@ export const teams = [
     name: "Scotland",
     flag: "scotland"
   },
-
-  {
-    id: "brazil",
-    imageUrl: teamImages.maracana,
-    imageAlt: "Maracana",
-    name: "Brazil",
-    flag: "brazil"
-  },
-  {
-    id: "argentina",
-    imageUrl: teamImages.laBombonera,
-    imageAlt: "La bombonera",
-    name: "Argentina",
-    flag: "argentina"
-  },
-  {
-    id: "uruguay",
-    imageUrl: teamImages.uruguay,
-    imageAlt: "Penarol`s stadium",
-    name: "Uruguay",
-    flag: "uruguay"
-  },
-  {
-    id: "paraguay",
-    imageUrl: teamImages.defensores,
-    imageAlt: "Defensores",
-    name: "Paraguay",
-    flag: "paraguay"
-  },
-  {
-    id: "ecuador",
-    imageUrl: teamImages.monumental,
-    imageAlt: "Monumental",
-    name: "Ecuador",
-    flag: "ecuador"
-  },
-  {
-    id: "colombia",
-    imageUrl: teamImages.risaralda,
-    imageAlt: "Pereira, Risaralda stadium",
-    name: "Colombia",
-    flag: "colombia"
-  },
-
   {
     id: "czech-republic",
     imageUrl: teamImages.czechRepublic,
@@ -156,5 +112,47 @@ export const teams = [
     imageAlt: "Ernst Happel Stadion",
     name: "Austria",
     flag: "austria"
+  },
+  {
+    id: "brazil",
+    imageUrl: teamImages.maracana,
+    imageAlt: "Maracana",
+    name: "Brazil",
+    flag: "brazil"
+  },
+  {
+    id: "argentina",
+    imageUrl: teamImages.laBombonera,
+    imageAlt: "La bombonera",
+    name: "Argentina",
+    flag: "argentina"
+  },
+  {
+    id: "uruguay",
+    imageUrl: teamImages.uruguay,
+    imageAlt: "Penarol`s stadium",
+    name: "Uruguay",
+    flag: "uruguay"
+  },
+  {
+    id: "paraguay",
+    imageUrl: teamImages.defensores,
+    imageAlt: "Defensores",
+    name: "Paraguay",
+    flag: "paraguay"
+  },
+  {
+    id: "ecuador",
+    imageUrl: teamImages.monumental,
+    imageAlt: "Monumental",
+    name: "Ecuador",
+    flag: "ecuador"
+  },
+  {
+    id: "colombia",
+    imageUrl: teamImages.risaralda,
+    imageAlt: "Pereira, Risaralda stadium",
+    name: "Colombia",
+    flag: "colombia"
   },
 ];
